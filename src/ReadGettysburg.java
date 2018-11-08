e8f0218547adff866dcce344df8a8fb9c4719e94
jjacobson20 <jjacobson20@HS-MATH2-09.gvsd.org>
2018-11-08 13:36:15 -0500

    Word parsing

--- src/ReadGettysburg.java
+++ src/ReadGettysburg.java
@@ -10,6 +10,8 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
 public class ReadGettysburg
 {
 	
@@ -24,35 +26,49 @@ public class ReadGettysburg
 			wordList.add(word);
 		}
 		
+		int allLengths = 0;
 		String longestWord = wordList.get(1);
-		for(String currentWord : wordList)
+		//for(String currentWord : wordList)
+		for(int c = 0; c < wordList.size(); c++)
 		{
+			
+			String currentWord = wordList.get(c);
+			if(currentWord.contains("--"))
+			{
+				currentWord = currentWord.replace("--", " ");
+				String[] separatedWords = currentWord.split(" ");
+				System.out.println(separatedWords.length);
+				
+				// split words connected by double hyphen
+				// into an array and replace the old combined
+				// word with the split words
+				/*
+				for(int i = 0; i < separatedWords.length; i++)
+				{
+					wordList.add(c + i, separatedWords[i]);
+				}
+				*/
+			}
+			
+			// remove punctuation
+			currentWord = currentWord.replace("-", "");
+			currentWord = currentWord.replace(",", "");
+			currentWord = currentWord.replace(".", "");
+			currentWord = currentWord.replace(";", "");
+			
+			allLengths += currentWord.length();
 			if(currentWord.length() > longestWord.length())
 			{
 				longestWord = currentWord;
 			}
 		}
 		
-		int allLengths = 0;
-		for(String currentWord : wordList)
-		{
-			allLengths += currentWord.length();
-		}
-		float average = allLengths / wordList.size();
-		
+		float average = (float)allLengths / (float)wordList.size();
+		System.out.println("Word count: " + wordList.size());
 		System.out.println("Average length: " + average);
 		System.out.println("Longest word: " + longestWord);
 		
 		file.close();
 		
-	}
-	
-	public String parseWord(String word)
-	{
-		if(word.contains("-"))
-		{
-			
-		}
-		return "y";
 	}
 }
